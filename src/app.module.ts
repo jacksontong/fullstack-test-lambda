03a5df5298e6ffa88072180e047b0378794a5b40
Jackson <tongtoan2704@gmail.com>
2021-03-23 11:26:53 +0700

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule, ConfigModule.forRoot()],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
